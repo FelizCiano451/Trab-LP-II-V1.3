7365ee1058bbb05df992ae02e5a58130d385a037
FelizCiano451 <barryallen4223@gmail.com>
2025-06-18 00:06:26 -0300

    Update CompactadorZip.java

--- GerenciadorBibliotecaPDF/src/util/CompactadorZip.java
+++ GerenciadorBibliotecaPDF/src/util/CompactadorZip.java
@@ -1,1 +1,27 @@
-// Conteúdo de exemplo para CompactadorZip.java
+package util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class CompactadorZip {
+    public static void compactarArquivo(String caminhoArquivo, String caminhoZip) throws Exception {
+        try (
+            FileOutputStream fos = new FileOutputStream(caminhoZip);
+            ZipOutputStream zipOut = new ZipOutputStream(fos);
+            FileInputStream fis = new FileInputStream(new File(caminhoArquivo))
+        ) {
+            File fileToZip = new File(caminhoArquivo);
+            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+            zipOut.putNextEntry(zipEntry);
+            
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+        }
+    }
+}
