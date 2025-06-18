6af379b230383528dfead49e845c3dac469df638
FelizCiano451 <barryallen4223@gmail.com>
2025-06-17 23:49:57 -0300

    Update NotaDeAula.java

--- GerenciadorBibliotecaPDF/src/model/NotaDeAula.java
+++ GerenciadorBibliotecaPDF/src/model/NotaDeAula.java
@@ -1,51 +1,32 @@
 package model;
 
-import java.util.List;
-import java.util.Arrays;
+public class NotaDeAula implements Entrada {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String curso;
 
-public class NotaDeAula extends EntradaPDF {
-    private String subtitulo;
-    private String disciplina;
-    private String instituicao;
-    private int numPaginas;
-
-    public NotaDeAula(String titulo, List<String> autores, String pathPDF,
-                      String subtitulo, String disciplina, String instituicao, int numPaginas) {
-        super(titulo, autores, pathPDF);
-        this.subtitulo = subtitulo;
-        this.disciplina = disciplina;
-        this.instituicao = instituicao;
-        this.numPaginas = numPaginas;
-    }
-
-    public static NotaDeAula fromLinha(String linha) {
-        String[] partes = linha.split(";");
-        String titulo = partes[1];
-        List<String> autores = Arrays.asList(partes[2].split(","));
-        String pathPDF = partes[3];
-        String subtitulo = partes[4];
-        String disciplina = partes[5];
-        String instituicao = partes[6];
-        int paginas = Integer.parseInt(partes[7]);
-        return new NotaDeAula(titulo, autores, pathPDF, subtitulo, disciplina, instituicao, paginas);
+    public NotaDeAula(String titulo, String autor, int ano, String curso) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.curso = curso;
     }
 
     @Override
-    public String toLinha() {
-        return String.join(";",
-            "NOTA",
-            titulo,
-            String.join(",", autores),
-            pathPDF,
-            subtitulo,
-            disciplina,
-            instituicao != null ? instituicao : "",
-            String.valueOf(numPaginas)
-        );
-    }
+    public String getTitulo() { return titulo; }
 
     @Override
-    public String getTipo() {
-        return "Nota de Aula";
+    public String getAutor() { return autor; }
+
+    @Override
+    public int getAno() { return ano; }
+
+    public String getCurso() { return curso; }
+
+    @Override
+    public String gerarBibTeX() {
+        return String.format("@misc{%s,\n  author = {%s},\n  title = {%s},\n  year = {%d},\n  note = {Nota de aula do curso: %s}\n}",
+            titulo.replaceAll("\\s", ""), autor, titulo, ano, curso);
     }
 }
