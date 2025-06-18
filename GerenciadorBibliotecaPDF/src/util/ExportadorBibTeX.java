4299a699192d8c6966751f97918c48bb350ad862
FelizCiano451 <barryallen4223@gmail.com>
2025-06-18 00:06:02 -0300

    Update ExportadorBibTeX.java

--- GerenciadorBibliotecaPDF/src/util/ExportadorBibTeX.java
+++ GerenciadorBibliotecaPDF/src/util/ExportadorBibTeX.java
@@ -1,1 +1,18 @@
-// Conteúdo de exemplo para ExportadorBibTeX.java
+package util;
+
+import model.Entrada;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class ExportadorBibTeX {
+    public static void exportarParaArquivo(List<Entrada> entradas, String caminhoArquivo) throws IOException {
+        try (FileWriter writer = new FileWriter(caminhoArquivo)) {
+            for (Entrada entrada : entradas) {
+                writer.write(entrada.gerarBibTeX());
+                writer.write("\n\n");
+            }
+        }
+    }
+}
