9a36bce031e4286b5b072928b86c06757bf2d046
FelizCiano451 <barryallen4223@gmail.com>
2025-06-18 00:07:17 -0300

    Update PersistenciaEmArquivo.java

--- GerenciadorBibliotecaPDF/src/util/PersistenciaEmArquivo.java
+++ GerenciadorBibliotecaPDF/src/util/PersistenciaEmArquivo.java
@@ -1,1 +1,21 @@
-// Conteúdo de exemplo para PersistenciaEmArquivo.java
+package util;
+
+import model.Entrada;
+
+import java.io.*;
+import java.util.List;
+
+public class PersistenciaEmArquivo {
+    public static void salvar(List<Entrada> entradas, String caminho) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(caminho))) {
+            oos.writeObject(entradas);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<Entrada> carregar(String caminho) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(caminho))) {
+            return (List<Entrada>) ois.readObject();
+        }
+    }
+}
